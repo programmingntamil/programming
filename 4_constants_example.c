b9eb5b67abfce24fc5c8f14c2ae0447ee8ee0549
programmingntamil <43327070+programmingntamil@users.noreply.github.com>
2019-11-14 16:44:09 +0530

    Create 4_constants_example.c

---
+++ 4_constants_example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const float pi = 3.14;
+    int radius = 5;
+
+    float area = pi * radius * radius;
+
+    printf("Area of Circle is %f",area);
+    return 0;
+}
