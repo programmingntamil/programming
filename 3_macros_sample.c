18894c9a72fa70b5e6d1721f4603d487a97d4fc6
programmingntamil <43327070+programmingntamil@users.noreply.github.com>
2019-11-14 15:45:52 +0530

    Create 3_macros_sample.c

---
+++ 3_macros_sample.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // First Method of including header file
+#include "stdlib.h"// Secod method of including header file
+
+#define AGE 50
+int main()
+{
+    printf("AGE VALUE IS %d",AGE);
+    return 0;
+}
